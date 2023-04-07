b79a3597bb23f3e42fa842377196322a70c02d6e
siddharthverma314 <siddharthverma314@gmail.com>
2023-04-07 19:53:06 +0000

    Update from https://github.com/siddharthverma314/cv/commit/5580f84a316e94e8e708bbe16eca22c06c16bc63

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+var content;
+
 function update() {
-    is = Array.from(document.getElementsByTagName('input')).map(e=>e.checked);
-    cs = Array.from(document.getElementById('content').children);
-    cs.forEach(e => {
-        e.hidden = !is[Number.parseInt(e.dataset["content"])];
-    });
+    is = Array.from(document.getElementsByTagName('input')).map(c => c.checked);
+    document.getElementById("content").replaceChildren(...content.filter(e => is[Number.parseInt(e.dataset["content"])]));
 }
 
 function init() {
+    content = Array.from(document.getElementById("content").children);
+    // check the correct types
     is = Array.from(document.getElementsByTagName('input'));
     is[0].checked = true;
     is[1].checked = true;
